656a66b4b9f10d411f6a0965aedb2f3aa3c854bd
SergeySerzhan <s.sergey96@mail.ru>
2022-06-01 16:34:44 +0300

    feat: complete zip tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import { createWriteStream } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const pathToFile = resolve(dirname(fileURLToPath(import.meta.url)), './files/fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+    const writeStr = createWriteStream(pathToFile, {encoding: 'utf-8'});
+    process.stdin.on('data', data => {
+        writeStr.write(data);
+    })
 };
+
+write();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,21 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
+const pathToDir = dirname(fileURLToPath(import.meta.url));
+const pathToFile = resolve(pathToDir, './files/fileToCompress.txt');
+const pathToArchive = resolve(pathToDir, './files/archive.gz');
+
 export const decompress = async () => {
-    // Write your code here 
+    const source = createReadStream(pathToArchive);
+    const unzip = createGunzip();
+    const dest = createWriteStream(pathToFile);
+
+    pipeline(source, unzip, dest, (e) => {
+        if (e) console.error('Error occurred');
+    })
 };
+
+decompress();
